a35a0786d68f101a6ca258a83a501fbb6410d645
Dark Piston <56780242+darkpiston@users.noreply.github.com>
2026-03-20 21:56:51 -0700

    Add iOS Scaffolding

---
+++ ios/Money/MoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyApp.swift
+//  Money
+//
+//  Created by Jacob Pope on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct MoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
